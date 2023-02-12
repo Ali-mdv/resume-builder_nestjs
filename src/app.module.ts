b84c75dc73a9b25121749d6db65f2a5a0f8361cf
ali <alimdv74.am@outlook.com>
2023-02-12 11:51:04 +0330

    create contact module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ResumeModule } from './resume/resume.module';
+import { ContactModule } from './contact/contact.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, ResumeModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, ResumeModule, ContactModule],
 })
 export class AppModule {}
